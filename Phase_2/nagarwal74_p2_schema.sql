667889e257836cdb9abada28946e158b1249c35f
AGARWAL, NIKHIL <Nikhil.Agarwal@capitalone.com>
2022-07-02 14:46:24 -0400

    updated docs

--- Phase_2/nagarwal74_p2_schema.sql
+++ Phase_2/nagarwal74_p2_schema.sql
@@ -3,9 +3,9 @@ $do$
 BEGIN
    IF NOT EXISTS (
       SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
-      WHERE  rolname = 'gatechUser') THEN
+      WHERE  rolname = 'team065User') THEN
 
-      CREATE ROLE gatechUser LOGIN PASSWORD 'gatech123';
+      CREATE ROLE gatechUser LOGIN PASSWORD 'team065123';
    END IF;
 END
 $do$;
@@ -27,8 +27,8 @@ CREATE TABLE if not exists "USER" (
     nickname VARCHAR(50) NOT NULL,
     "password" VARCHAR(50) NOT NULL, 
     postal_code VARCHAR(50) NOT null,
-    UNIQUE(postal_code),
-    CONSTRAINT fk_Address_postal_code_User_postal_code
+    UNIQUE(nickname),
+    CONSTRAINT fk_User_Address_postal_code
       FOREIGN KEY(postal_code) 
       REFERENCES "Address"(postal_code)
  );
@@ -41,29 +41,24 @@ CREATE table if not exists Address (
     latitude  DECIMAL(12,9) NOT NULL
  );
 
- CREATE TABLE if not exists ItemListing (
+ CREATE TABLE if not exists Item (
   email VARCHAR(10) NOT NULL,
-  item_number INT NOT NULL,
+  item_number INT AUTO_INCREMENT PRIMARY KEY ,
   name_title VARCHAR(250) NOT NULL,
   "condition" VARCHAR(50) NOT NULL,
   "description" VARCHAR(250) NULL,
-  primary key (item_number),
-  CONSTRAINT fk_ItemListing_email_User_email
+  CONSTRAINT fk_Item_User_email
     FOREIGN KEY(email) 
-    REFERENCES "USER"(email),
-  UNIQUE(item_number)
+    REFERENCES "USER"(email)
 );
 
 
 CREATE TABLE if not exists ComputerGame (
     item_number INT NOT NULL,
-    platform_id VARCHAR(250) NOT NULL,
-    CONSTRAINT fk_ComputerGame_item_number_ItemListing_item_number
+    platform VARCHAR(250) NOT NULL,
+    CONSTRAINT fk_ComputerGame_Item_item_number
       FOREIGN KEY(item_number) 
-      REFERENCES ItemListing (item_number),
-    CONSTRAINT fk_CG_platform_Platform_platform_name
-      FOREIGN KEY(platform_id) 
-      REFERENCES CG_Platform(platform_id),
+      REFERENCES Item (item_number),
     UNIQUE(item_number)
  );
 
@@ -71,9 +66,9 @@ CREATE TABLE if not exists ComputerGame (
 CREATE TABLE if not exists CollectibleCardGame (
     item_number INT NOT NULL,
     no_of_cards INT NOT NULL,
-    CONSTRAINT fk_CollectibleCardGame_item_number_ItemListing_item_number
+    CONSTRAINT fk_CollectibleCardGame_Item_item_number
       FOREIGN KEY(item_number) 
-      REFERENCES ItemListing (item_number),
+      REFERENCES Item (item_number),
     UNIQUE(item_number)
  );
 
@@ -82,47 +77,33 @@ CREATE TABLE if not exists VG_Platform (
     platform_name VARCHAR(250) NOT NULL
     );
 
-CREATE TABLE if not exists CG_Platform (  
-    platform_id INT primary KEY,
-    platform_name VARCHAR(250) NOT NULL
-    );
-
-CREATE TABLE if not exists media (  
-    media_id INT primary KEY,
-    media_name VARCHAR(250) NOT NULL
-    );
-
 CREATE TABLE if not exists VideoGame (
     item_number INT NOT NULL,
     platform_id  VARCHAR(250) NOT NULL,
-    media_id VARCHAR(250) NOT NULL,
-    CONSTRAINT fk_VideoGame_item_number_ItemListing_item_number
+    media VARCHAR(250) NOT NULL,
+    CONSTRAINT fk_VideoGame_Item_item_number
       FOREIGN KEY(item_number) 
-      REFERENCES ItemListing (item_number),
-    CONSTRAINT fk_Video_platform_Platform_platform_name
+      REFERENCES Item (item_number),
+    CONSTRAINT fk_Video_Platform_platform_id
       FOREIGN KEY(platform_id) 
       REFERENCES VG_Platform(platform_id),
-    CONSTRAINT fk_media
-      FOREIGN KEY(media_id) 
-      REFERENCES media(media_id),
     UNIQUE(item_number)
  );
 
 
 
 CREATE TABLE if not exists Trade (
-    proposed_item_number INT ,
-    desired_item_number  INT ,
-    TradeId  INT ,
+    proposed_item_number INT NOT NULL,
+    desired_item_number  INT NOT NULL,
     proposed_date TIMESTAMP NOT NULL,
-    accept_or_reject_date TIMESTAMP not null,
+    accept_or_reject_date TIMESTAMP null,
   	trade_status VARCHAR(8) null,
-    CONSTRAINT fk_trade_proposed_item_number_id_ItemListing_item_number_id
+    CONSTRAINT fk_trade_proposed_Item_item_number_id
         FOREIGN KEY(proposed_item_number)
-        REFERENCES ItemListing(item_number),
-    CONSTRAINT fk_trade_desired_item_number_id_ItemListing_item_number_id
+        REFERENCES Item(item_number),
+    CONSTRAINT fk_trade_desired_Item_item_number_id
         FOREIGN KEY(desired_item_number)
-        REFERENCES ItemListing(item_number),
-    primary KEY(TradeId)
+        REFERENCES Item(item_number),
+        UNIQUE(proposed_item_number,desired_item_number)
 );
 
